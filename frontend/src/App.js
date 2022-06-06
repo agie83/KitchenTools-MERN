c5adb3a631bdebe2368594824e646f0c76883a35
agie83 <agnes.agocs@gmail.com>
2022-06-06 19:48:27 +0200

    frontend, routes from menuList (draft; protectedRoutes and others not yet)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,43 @@
 import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { v4 as uuid } from 'uuid';
 import LayoutPage from './pages/LayoutPage';
+import { menu } from './utils/menus';
+import 'bootstrap/dist/js/bootstrap';
 
 function App() {
+  const routeList = [];
+  menu.map((menuItem) => {
+    routeList.push(menuItem);
+    if (menuItem.submenu.length > 0) {
+      menuItem.submenu.map((submenu) => {
+        routeList.push(submenu);
+
+        return routeList;
+      });
+    }
+    if (menuItem.routePathChildren?.length > 0) {
+      menuItem.routePathChildren.map((pathChild) => {
+        const childRoute = { ...menuItem };
+        routeList.push(childRoute, childRoute.routePath = pathChild, childRoute._id = uuid());
+
+        return routeList;
+      });
+    }
+    return routeList;
+  });
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LayoutPage />} />
+        {
+          routeList.map((route) => (
+            <Route
+              key={uuid()}
+              path={route.routePath}
+              element={<LayoutPage navData={menu} />}
+            />
+          ))
+        }
       </Routes>
     </BrowserRouter>
   );
