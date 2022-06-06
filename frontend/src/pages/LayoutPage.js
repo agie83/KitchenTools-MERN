a231a90f965079bad39d8b1d32bbaaec5009dd0d
agie83 <agnes.agocs@gmail.com>
2022-06-06 19:15:31 +0200

    frontend layout system draft

--- frontend/src/pages/LayoutPage.js
+++ frontend/src/pages/LayoutPage.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import logo from '../logo.svg';
+import Layout from './Layout';
 
-function LayoutPage() {
+function LayoutPage({ navData }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout navData={navData} />
   );
 }
 
