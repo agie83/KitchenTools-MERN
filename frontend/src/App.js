e171cc7492ed14e7ea9353faaf7981d000366c0c
agie83 <agnes.agocs@gmail.com>
2022-06-16 13:36:13 +0200

    create ProtectedRoute handler

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
 import { v4 as uuid } from 'uuid';
 import AuthProvider from './contexts/AuthContext';
+import CartProvider from './contexts/CartContext/CartContext';
 import LayoutPage from './pages/LayoutPage';
 import { menu } from './utils/menus';
 import 'bootstrap/dist/js/bootstrap';
+import ProtectedRoute from './utils/ProtectedRoute';
 
 function App() {
   const routeList = [];
@@ -30,17 +33,33 @@ function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          {
-          routeList.map((route) => (
-            <Route
-              key={uuid()}
-              path={route.routePath}
-              element={<LayoutPage navData={menu} />}
-            />
-          ))
-        }
-        </Routes>
+        <CartProvider>
+          <Routes>
+            {
+           (routeList) && routeList.map((route) => (
+             (!route.protected)
+               ? (
+                 <Route
+                   key={uuid()}
+                   path={route.routePath}
+                   element={<LayoutPage navData={menu} />}
+                 />
+               )
+               : (
+                 <Route
+                   key={route._id}
+                   path={route.routePath}
+                   element={(
+                     <ProtectedRoute>
+                       <LayoutPage navData={menu} />
+                     </ProtectedRoute>
+                  )}
+                 />
+               )
+           ))
+           }
+          </Routes>
+        </CartProvider>
       </AuthProvider>
     </BrowserRouter>
   );
