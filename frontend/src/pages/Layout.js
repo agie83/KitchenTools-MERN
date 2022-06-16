625e82ac9b3f00687dff2218ee4412abc9a502b9
agie83 <agnes.agocs@gmail.com>
2022-06-16 13:50:47 +0200

    update with NotFoundPage handling

--- frontend/src/pages/Layout.js
+++ frontend/src/pages/Layout.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { v4 as uuid } from 'uuid';
 import Navbar from '../components/Navbar/Navbar';
 import Footer from '../components/Footer/Footer';
 import ShopNavbar from '../components/ShopNavbar/ShopNavbar';
 import './layout.scss';
+import { AuthContext } from '../contexts/AuthContext';
+import { CartContext } from '../contexts/CartContext/CartContext';
+import getItems from '../utils/queries';
+import { CART_URL } from '../constants/url';
 
 const Layout = ({ children, navData }) => {
+  const { token } = useContext(AuthContext);
+  const [cartItems, setCartItems] = useContext(CartContext);
+  const [cartList, setCartList] = useState([]);
+
+  // useEffect(() => {
+  //   getItems(CART_URL, {
+  //     method: 'GET',
+  //     headers: {
+  //       authorization: `Bearer ${token}`,
+  //     },
+  //   }).then((data) => {
+  //     setCartList(data.cartItems);
+  //     setCartItems(data.cartItems);
+  //   });
+  // }, []);
+
   const path = `/${window.location.pathname.split('/')[1]}` || '/';
   const routeList = [];
 
@@ -20,7 +40,9 @@ const Layout = ({ children, navData }) => {
     return routeList;
   });
 
-  const currentPage = routeList.filter((navItem) => path === navItem.navLink)[0];
+  const page404 = routeList.filter((item) => item.routePath === '*')[0];
+  const currentPage = routeList.filter((navItem) => path === navItem.navLink)[0] || page404;
+
   const mainContent = (currentPage?.contentComponents)
     ? currentPage.contentComponents.mainContent : [];
   const asideContent = (currentPage?.contentComponents)
