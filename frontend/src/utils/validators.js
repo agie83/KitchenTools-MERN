db19ae8b6c582715d8bcd8cbc71f624e35ac4866
agie83 <agnes.agocs@gmail.com>
2022-06-17 21:21:45 +0200

    frontend, validators for profile (data-and password modification)

--- frontend/src/utils/validators.js
+++ frontend/src/utils/validators.js
@@ -69,3 +69,49 @@ export const registerSchema = Joi.object({
           'any.only': 'A két jelszó eltér',
         }),
 });
+
+export const profileSchema = Joi.object({
+  firstName:
+  Joi.string()
+    .required().min(2).max(30)
+    .messages({
+      'string.min': 'Vezetéknév: Minumum 2 karakter',
+      'string.max': 'Vezetéknév: Maximum 30 karakter',
+      'string.empty': 'Keresztnév megadása kötelező',
+      'any.required': 'Keresztnév megadása kötelező',
+    }),
+  lastName:
+    Joi.string()
+      .required().min(2).max(20)
+      .messages({
+        'string.min': 'Vezetéknév: Minumum 2 karakter',
+        'string.max': 'Vezetéknév: Maximum 20 karakter',
+        'string.empty': 'Vezetéknév megadása kötelező',
+        'any.required': 'Vezetéknév megadása kötelező',
+      }),
+});
+
+export const passwordSchema = Joi.object({
+
+  password:
+      Joi.string()
+        .min(8)
+        .required()
+        .messages({
+          'string.min': 'Jelszó: Minumum 8 karakter megadása kötelező',
+          'string.empty': 'Jelszó megadása kötelező',
+          'any.required': 'Jelszó megadása kötelező',
+
+        }),
+  passwordAgain:
+      Joi.string()
+        .min(8)
+        .required()
+        .valid(Joi.ref('password'))
+        .messages({
+          'string.min': 'Jelszó: Minumum 8 karakter megadása kötelező',
+          'string.empty': 'Jelszó újbóli megadása kötelező',
+          'any.required': 'Jelszó újbóli megadása kötelező',
+          'any.only': 'A két jelszó eltér',
+        }),
+});
