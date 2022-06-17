e461ad95b16fe1208632d07f846ffebafb182662
agie83 <agnes.agocs@gmail.com>
2022-06-17 17:10:00 +0200

    minor changes

--- frontend/src/components/Informations/Informations.js
+++ frontend/src/components/Informations/Informations.js
@@ -5,20 +5,17 @@ function Informations() {
     <section className="container-fluid bg-light p-5">
       <h1 className="h4 mb-5">Fiztetési-és átvételi lehetőségek</h1>
       <h2 className="h5 mb-3">Fizetési módok</h2>
-      <p>
-        <ul>
-          <li>Bankkártyával a helyszínen</li>
-          <li>Készpénzzel a helyszínen</li>
-          <li>Átutalással</li>
-        </ul>
-      </p>
+      <ul>
+        <li>Bankkártyával a helyszínen</li>
+        <li>Készpénzzel a helyszínen</li>
+        <li>Átutalással</li>
+      </ul>
 
       <h2 className="h5 mb-3">Átvételi lehetőségek</h2>
-      <p>
-        <ul>
-          <li>Üzleteinkben</li>
-        </ul>
-      </p>
+
+      <ul>
+        <li>Üzleteinkben</li>
+      </ul>
 
     </section>
   );
