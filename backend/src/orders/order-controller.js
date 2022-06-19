32bbd51af779fbf4dda0912ce0599f3ad36a4b50
agie83 <agnes.agocs@gmail.com>
2022-06-19 11:08:46 +0200

    delete unused functions; rename setStatusToPaid to setStatusToOrdered

--- backend/src/orders/order-controller.js
+++ backend/src/orders/order-controller.js
@@ -10,38 +10,9 @@ class OrderController {
     }
   }
 
-  static async post(req, res, next) {
-    try {
-      const response = await OrderService.addToCart(req.params, req.body);
-      res.status(200).json(response);
-    } catch (err) {
-      next(err);
-    }
-  }
-
-  static async deleteOne(req, res, next) {
-    try {
-      const isDeleted = await OrderService.deleteOrder(req.params.orderId, req.user.id);
-      const message = isDeleted ? 'Deleted successfully' : 'Already deleted';
-      res.status(200).send({ message });
-    } catch (e) {
-      next(e);
-    }
-  }
-
-  static async deleteMany(req, res, next) {
-    try {
-      const amountDeleted = await OrderService.deleteAllOrder(req.user.id);
-      const message = amountDeleted === 0 ? 'Already deleted' : `Deleted ${amountDeleted} orders successfully`;
-      res.status(200).send({ message });
-    } catch (e) {
-      next(e);
-    }
-  }
-
   static async updateMany(req, res, next) {
     try {
-      const newOrders = await OrderService.setStatusToPaid(req.user.id);
+      const newOrders = await OrderService.setStatusToOrdered(req.user.id);
       res.status(200).send({ orders: newOrders });
     } catch (e) {
       next(e);
