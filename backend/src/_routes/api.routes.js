377063f3285477ac8ec3a359c0c0d3dfd457825b
agie83 <agnes.agocs@gmail.com>
2022-06-20 00:49:37 +0200

    add categories

--- backend/src/_routes/api.routes.js
+++ backend/src/_routes/api.routes.js
@@ -7,6 +7,7 @@ import shop from '../shop/shop-routes';
 import cart from '../cart/cart-routes';
 import orders from '../orders/order-routes';
 import user from '../user/user-routes';
+import categories from '../categories/category-routes';
 
 const router = express.Router();
 
@@ -16,6 +17,7 @@ router.use(express.json());
 router.use(register);
 router.use(login);
 router.use(shop);
+router.use(categories);
 router.use(authorizationMiddleware);
 router.use(cart);
 router.use(orders);
